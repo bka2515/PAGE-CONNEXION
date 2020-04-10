b4e73ce0b4c957004c2b67ba30d4f6f6e416d4a7
bka2515 <63196741+bka2515@users.noreply.github.com>
2020-04-10 18:54:42 +0200

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8" />
+      <link rel="stylesheet" href="cssconnexion/connect.css">
+      <style>
+      </style>
+   </head>
+   <body>
+      <div id="tof">
+      <div class="icon1">
+         <img id="img3" src="images/logo-QuizzSA.png" alt="">
+            <h1>Le plaisir de jouer</h1>
+         </div>
+               <div class="prtie"> 
+    <div 
+             class="tete"><h3>Login Form<h3>
+    </div>
+                <input class="log" type="text" name="Login" value="Login"
+                img  src="images/ic-login.png" alt="">
+                <br>
+                
+                <input class="pwd" type="text" name="Password" value="Password"><br>
+                <img id="imgf" src="images/ic-password.png" alt="">
+                <input class="connect" type="submit" name="connexion" value="connexion">
+                <p class="pragra">S'inscrire pour jouer?</p>
+               </div>
+
+      </div>
+   </body>
+</html>
